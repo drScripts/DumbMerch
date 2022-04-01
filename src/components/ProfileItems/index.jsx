3bdd985038d34547e6afb2c3f66431df3b803d39
Nathanael <nathanael.vd@gmail.com>
2022-04-02 05:58:36 +0700

    change the class for detail product become pre

--- src/components/ProfileItems/index.jsx
+++ src/components/ProfileItems/index.jsx
@@ -4,7 +4,10 @@ const ProfileItems = ({ title = "", value = "" }) => {
   return (
     <div className="mb-4">
       <h4 className="text-orange">{title}</h4>
-      <h5 className="text-light">{value}</h5>
+      <h5 className="text-light">
+        {value}
+        {"\n "}
+      </h5>
     </div>
   );
 };
